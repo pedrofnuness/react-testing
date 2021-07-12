9dd7ce39b798729ad6a04d13a8cf71ad4dfcb9d3
pedrofnuness <pedro9661@gmail.com>
2021-07-12 11:12:56 -0300

    first commit

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,17 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    let answer = true;
+    if (answer === false) {
+      return 0;
+    } else {
+      return 10;
+    }
+  }, []);
 
   return (
-    <div data-test="component-app">
+    <div data-test="component-app" className="App">
       <h1 data-test="counter-display">
         The counter is currently&nbsp;
         <span data-test="count">{count}</span>
       </h1>
-      <button data-test="increment-button" onClick={() => setCount(count + 1)}>
+
+      <div
+        data-test="error-message"
+        className={`error ${error ? "" : "hidden"}`}
+      >
+        The counter cannot go below 0
+      </div>
+      <button
+        data-test="increment-button"
+        onClick={() => {
+          if (error) {
+            setError(false);
+          }
+          setCount(count + 1);
+        }}
+      >
         Increment counter
+      </button>
+      <button
+        data-test="decrement-button"
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1);
+          } else {
+            setError(true);
+          }
+        }}
+      >
+        Decrement counter
       </button>
     </div>
   );
