f920e2f1087badacbf27f77ab3ca1c1561267c64
pedrofnuness <pedro9661@gmail.com>
2021-11-15 17:17:23 -0300

    Add proptypes for Congrats component

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,10 +1,12 @@
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-import { findByTestAttr } from "../test/testUtils";
+import { checkProps, findByTestAttr } from "../test/testUtils";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { success: false };
 
 /**
  * Factory function to create a ShallowWrapper for the Congrats component.
@@ -13,7 +15,8 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("renders without error", () => {
@@ -32,4 +35,8 @@ test("renders non-empty congrats message when `success` prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
