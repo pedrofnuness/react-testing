aeabda15a998fea9e81e167689612486bba7cbe8
pedrofnuness <pedro9661@gmail.com>
2022-01-20 22:11:17 -0300

    feat: created tests for input component

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -25,3 +25,18 @@ test("does not throw warning with expected props", () => {
   const expectedProps = { secretWord: "file" };
   checkProps(Input, expectedProps);
 });
+
+describe("state controlled input field", () => {
+  test("state updates with value of input box upon change", () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+});
