e051fc93735509a8169957b062cea557ca73edd6
pedrofnuness <pedronunes.dev@gmail.com>
2022-01-24 13:54:55 -0300

    Basis for app component

---
+++ jotto/src/App.test.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import {findByTestAttr} from '../test/testUtils';
+import App from './App';
+
+/**
+ * Setup function for App component.
+ * @returns {ShallowWrapper}
+ */
+
+const setup = () => {
+  return shallow(<App />);
+}
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent).toHaveLength(1);
+})
