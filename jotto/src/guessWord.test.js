87e3a00ddac6c29c4c19c0e5232f66a83d3c790a
pedrofnuness <pedronunes.dev@gmail.com>
2022-01-25 10:11:46 -0300

    Skipping tests for guess word

--- jotto/src/guessWord.test.js
+++ jotto/src/guessWord.test.js
@@ -28,7 +28,7 @@ const setup = (state = {}) => {
   return wrapper;
 };
 
-describe('no words guessed', () => {
+describe.skip('no words guessed', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = setup({
@@ -44,10 +44,52 @@ describe('no words guessed', () => {
   });
 });
 
-describe('some words guessed', () => {
+describe.skip('some words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{ guessedWord: 'agile', letterMatchCount: 1}]
+    })
+  });
 
+  test('adds rows to GuessedWords table', () => {
+    const guessedWordsRows = findByTestAttr(wrapper);
+    expect(guessedWordsRows).toHaveLength(2);
+  })
 });
 
-describe('guess secret word', () => {
+describe.skip('guess secret word', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{ guessedWord: 'agile', letterMatchCount: 1}]
+    });
+    
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    const mockEvent = {target: { value: 'party' }};
+    inputBox.simulate('change', mockEvent);
 
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click', { preventDefault() {}});
+  });
+
+  test('adds row to guessedWords table', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNodes).toHaveLength(3);
+  });
+  test('displays congrats component', () => {
+    const congrats = findByTestAttr(wrapper, 'component-congrats');
+    expect(congrats.text().length).toBeGreaterThan(0);
+  });
+  test('does not display input component contents', () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    expect(inputBox.exists()).toBe(false);
+
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    expect(submitButton.exists()).toBe(false);
+  })
 })
