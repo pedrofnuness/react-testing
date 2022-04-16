fbeac4cd25badbb7eb67404c428d612f95ac8e5a
pedrofnuness <pedronunes.dev@gmail.com>
2022-04-16 18:56:57 -0300

    set up guesswords test

--- jotto/src/actions/index.test.js
+++ jotto/src/actions/index.test.js
@@ -1,12 +1,6 @@
 import moxios from 'moxios';
-import { getSecretWord, correctGuess, actionTypes } from './';
+import { getSecretWord } from './';
 
-describe('correctGuess', () => {
-  test('returns an action with type `CORRECT_GUESS`', () => {
-    const action = correctGuess();
-    expect(action).toStrictEqual({ type: actionTypes.CORRECT_GUESS});
-  })
-})
 
 describe('getSecretWord', () => {
   beforeEach(() => {
