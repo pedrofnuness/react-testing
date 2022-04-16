89e0bda5932e06190d19021c2d3ef18bf81df570
pedrofnuness <pedronunes.dev@gmail.com>
2022-04-16 19:04:24 -0300

    started implementing integration tests

--- jotto/src/integration.test.js
+++ jotto/src/integration.test.js
@@ -2,9 +2,26 @@ import { storeFactory } from '../test/testUtils';
 import { guessWord } from './actions';
 
 describe('guessWord action dispatcher', () => {
+  const secretWord = 'party';
+  const unsuccessfulGuess = 'train';
   describe('no guessed words', () => {
+    let store;
+    const initialState = { secretWord };
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
     test('updates state correctly for unsuccessful guess', () => {
-
+      store.dispatch(guessWord(unsuccessfulGuess));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [{
+          guessedWord: unsuccessfulGuess,
+          letterMatchCount: 3
+        }]
+      }
+      expect(newState).toEqual(expectedState);
     });
     test('updates state correctly for successful guess', () => {
 
